364ab6a4ff2a7232be6bbab664d81516a0b41d12
Vardan Petrossiantz <vardanpetrossiantz@gmail.com>
2021-10-31 10:09:51 +0100

    Update urlValidator.test.js

--- 3nd_evalutate_articles_using_nlp_udacity_project/evaluate-articles/src/test/urlValidator.test.js
+++ 3nd_evalutate_articles_using_nlp_udacity_project/evaluate-articles/src/test/urlValidator.test.js
@@ -1,5 +1,5 @@
 // test the url validator
-import{ validateUrl } from "..src/client/js/urlValidator.js"
+import{ validateUrl } from "../client/js/urlValidator"
 
 describe("Validate URL", () => {
 test('The input is analized when URL is valid', () => {
